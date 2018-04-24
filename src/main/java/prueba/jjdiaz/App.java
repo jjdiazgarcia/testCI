9ffb94290c5e4acd232e7fc66092d0df1030ae6c
Jeronimo Diaz <jeronimojose.diaz@bbva.com>
2018-04-24 16:17:25 +0200

    Start mvn app

---
+++ src/main/java/prueba/jjdiaz/App.java
@@ -0,0 +1,13 @@
+package prueba.jjdiaz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
